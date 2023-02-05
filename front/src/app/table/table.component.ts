d6fa8c9cc772c958deb88aef86e765127182c089
johnpsar <jps200010@gmail.com>
2023-02-05 09:47:56 +0200

    array sorting done

--- front/src/app/table/table.component.ts
+++ front/src/app/table/table.component.ts
@@ -12,7 +12,10 @@ export class TableComponent implements OnInit {
   characters: Character[] = [];
   loading: boolean = false;
   errorMessage: string = '';
-
+  nameAscending: boolean = true;
+  showsAscending: boolean = true;
+  gamesAscending: boolean = true;
+  sortField: 'name' | 'shows' | 'games' = 'name';
   constructor(private characterService: CharacterService) {}
 
   ngOnInit() {
@@ -29,5 +32,71 @@ export class TableComponent implements OnInit {
         this.errorMessage = error;
         this.loading = false;
       };
+  }
+
+  onSortClick(field: string) {
+    switch (field) {
+      case 'name': {
+        this.sortField = 'name';
+        this.nameAscending = !this.nameAscending;
+        this.gamesAscending = true;
+        this.showsAscending = true;
+        let that = this;
+        this.characters.sort(function (a: Character, b: Character) {
+          var keyA = a.name,
+            keyB = b.name;
+          console.log(keyA, keyB);
+          if (that.nameAscending) {
+            if (keyA < keyB) return -1;
+            if (keyA >= keyB) return 1;
+          } else {
+            if (keyA < keyB) return 1;
+            if (keyA >= keyB) return -1;
+          }
+          return 0;
+        });
+        break;
+      }
+      case 'shows': {
+        this.sortField = 'shows';
+        this.showsAscending = !this.showsAscending;
+        this.nameAscending = true;
+        this.gamesAscending = true;
+        let that = this;
+        this.characters.sort(function (a: Character, b: Character) {
+          var keyA = a.tvShows.length,
+            keyB = b.tvShows.length;
+          if (that.showsAscending) {
+            if (keyA < keyB) return -1;
+            if (keyA >= keyB) return 1;
+          } else {
+            if (keyA < keyB) return 1;
+            if (keyA >= keyB) return -1;
+          }
+          return 0;
+        });
+        break;
+      }
+      case 'games': {
+        this.sortField = 'games';
+        this.gamesAscending = !this.gamesAscending;
+        this.nameAscending = true;
+        this.showsAscending = true;
+        let that = this;
+        this.characters.sort(function (a: Character, b: Character) {
+          var keyA = a.videoGames.length,
+            keyB = b.videoGames.length;
+          if (that.gamesAscending) {
+            if (keyA < keyB) return -1;
+            if (keyA >= keyB) return 1;
+          } else {
+            if (keyA < keyB) return 1;
+            if (keyA >= keyB) return -1;
+          }
+          return 0;
+        });
+        break;
+      }
+    }
   }
 }
